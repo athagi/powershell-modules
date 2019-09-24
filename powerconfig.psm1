b7d27c6ba0899cbf69c36ce5830266cc1ddbaadb
hagita_a <hagita_a@worksap.co.jp>
2019-09-24 11:10:43 +0900

    add powercfg module

---
+++ powerconfig.psm1
@@ -0,0 +1,39 @@
+﻿function Test-DisableTurnOffHardDisk {
+  param(
+    [string]$SchemeGUID
+  )
+  $Val = (powercfg /Q $SchemeGUID SUB_DISK DISKIDLE | Select-String " AC ") -replace ".* AC .*: ", ""
+  if ($Val -eq "0x00000000") {
+    return $true
+  }
+  else {
+    return $false
+  }
+}
+
+function Test-DisableHybernate {
+  param(
+    [string]$SchemeGUID
+  )
+  $Val = (powercfg /Q $SchemeGUID SUB_SLEEP HIBERNATEIDLE | select-string " AC ") -replace ".* AC .*: ", ""
+  if ($Val -eq "0x00000000") {
+    return $true
+  }
+  else {
+    return $false
+  }
+}
+
+function Test-DisableSleep {
+  param(
+    [string]$SchemeGUID
+  )
+  $Val = (powercfg /Q $SchemeGUID SUB_SLEEP STANDBYIDLE | select-string " AC ") -replace ".* AC .*: ", ""
+  if ($Val -eq "0x00000000") {
+    return $true
+  }
+  else {
+    return $false
+  }
+}
+Export-ModuleMember -Function Test-DisableTurnOffHardDisk, Test-DisableHybernate, Test-DisableSleep
