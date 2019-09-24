87c22db8615a7d46fde8c67f8c1d9c2419c6e594
hagita_a <hagita_a@worksap.co.jp>
2019-09-24 19:36:53 +0900

    update service.psm1

--- service.psm1
+++ service.psm1
@@ -2,10 +2,11 @@
   Param(
     [parameter(Mandatory)][string]$Name
   )
-  $InstallProduct = Get-WmiObject Win32_Product | where-object {$_.Name -eq "$Name"}
-  if($InstallProduct) {
+  $InstallProduct = Get-WmiObject Win32_Product | where-object { $_.Name -eq "$Name" }
+  if ($InstallProduct) {
     return $true
-  } else {
+  }
+  else {
     return $false
   }
 }
@@ -14,13 +15,27 @@ function Test-Service {
   Param(
     [parameter(Mandatory = $true)][String]$Name
   )
-  $Service = Get-Service | Where-Object { $_.Name -eq "$Name"}
+  $Service = Get-Service | Where-Object { $_.Name -eq "$Name" }
 
-  if($Service) {
+  if ($Service) {
     return $true
-  } else {
+  }
+  else {
     return $false
   }
 }
 
-Export-ModuleMember -Function Test-Service,Test-Installed
+function Disable-Service {
+  param(
+    [parameter(Mandatory)][string]$ServiceName
+  )
+  $Service = Get-Service $ServiceName
+  if (!($Service)) {
+    return
+  }
+
+  Stop-Service -Name $ServiceName
+  Set-Service $ServiceName -StartupType Disabled
+}
+
+Export-ModuleMember -Function Test-Service, Test-Installed, Disable-Service
