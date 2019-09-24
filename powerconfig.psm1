8c754a95aa4f6df85bc8ce2baa5df2866698cf6a
hagita_a <hagita_a@worksap.co.jp>
2019-09-24 11:19:13 +0900

    add get-currentpowerplan

--- powerconfig.psm1
+++ powerconfig.psm1
@@ -36,4 +36,19 @@ function Test-DisableSleep {
     return $false
   }
 }
-Export-ModuleMember -Function Test-DisableTurnOffHardDisk, Test-DisableHybernate, Test-DisableSleep
+
+function Get-CurrentPowerPlan {
+  $Scheme = @{
+    "SCHEME_MAX"      = "a1841308-3541-4fab-bc81-f71556f20b4a";
+    "SCHEME_BALANCED" = "381b4222-f694-41f0-9685-ff5bb260df2e";
+    "SCHEME_MIN"      = "8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c"
+  }
+  $Mode = powercfg.exe /GETACTIVESCHEME
+  foreach ($k in $Scheme.Keys) {
+    if ($Mode -cmatch $Scheme[$k]) {
+      return $k
+    }
+  }
+}
+
+Export-ModuleMember -Function Test-DisableTurnOffHardDisk, Test-DisableHybernate, Test-DisableSleep, Get-CurrentPowerPlan
